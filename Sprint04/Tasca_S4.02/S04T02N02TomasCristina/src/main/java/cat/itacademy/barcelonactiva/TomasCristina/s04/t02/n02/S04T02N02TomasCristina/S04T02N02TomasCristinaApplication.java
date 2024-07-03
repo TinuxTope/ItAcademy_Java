2b66bc2f637178b020c45344dfd73729645170ae
TinuxTope <c.tomaspe@gmail.com>
2024-07-03 12:11:24 +0200

    4.2

---
+++ Sprint04/Tasca_S4.02/S04T02N02TomasCristina/src/main/java/cat/itacademy/barcelonactiva/TomasCristina/s04/t02/n02/S04T02N02TomasCristina/S04T02N02TomasCristinaApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.TomasCristina.s04.t02.n02.S04T02N02TomasCristina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N02TomasCristinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N02TomasCristinaApplication.class, args);
+	}
+
+}
---
+++ Sprint04/Tasca_S4.02/S04T02N02TomasCristina/src/test/java/cat/itacademy/barcelonactiva/TomasCristina/s04/t02/n02/S04T02N02TomasCristina/S04T02N02TomasCristinaApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.TomasCristina.s04.t02.n02.S04T02N02TomasCristina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T02N02TomasCristinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
