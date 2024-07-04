c1509d5ada4031c102a6e06de68b7ed387ed4865
TinuxTope <c.tomaspe@gmail.com>
2024-07-04 10:53:04 +0200

    inicio 5.1.n1

---
+++ Sprint05/src/main/java/S05T01N01TomasCristina/src/test/java/cat/itacademy/barcelonactiva/tomas/cristina/s05/t01/n01/S05T01N01TomasCristina/S05T01N01TomasCristinaApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.tomas.cristina.s05.t01.n01.S05T01N01TomasCristina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N01TomasCristinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
